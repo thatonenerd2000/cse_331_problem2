7f620319a1a19bf5e3b511201748db0c3092cbc6
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2022-04-11 17:19:31 -0400

    messing with code

--- Solution.py
+++ Solution.py
@@ -18,6 +18,11 @@ class Solution:
         but they must remain within the Solution class.
         """
         paths, bandwidths, priorities = {}, {}, {}
+
+        paths = bfs_path(self.graph, self.isp, self.info["list_clients"])
+        client_delays = Simulator.get_delays(self.info["list_clients"])
+
+        
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
         return (paths, bandwidths, priorities)
