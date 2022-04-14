b1de361a46d8724851000554dceec0db82d8bc4f
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2022-04-14 00:19:19 -0400

    4 out of 5 on input 1

--- Solution.py
+++ Solution.py
@@ -29,34 +29,53 @@ class Solution:
 
         return maxTuple
 
-    '''
-    param: self, start node, end node, graph
-    return: type:list a list of nodes in order from start to end
-    THIS IS NOT COMPLETE YET, NOT SURE IF THIS WILL WORK EITHER
-    Current Status: Stuck in infinite loop.
-    '''
-    def findPath(self,start,end,graph):
-        backTrackNode = -1
-        path = []
+    def maxBandList(self, clientList):
+        bandwidths = self.info["bandwidths"]
+        bandwidthsTuple = []
+        bandwidthsNode = []
 
-        #keeps track of the current node
-        currentNode = start
+        for client in clientList:
+            bandwidthsTuple.append((client,bandwidths[client]))
 
-        #Loops until reaches the end node
-        while(currentNode != end):
-            #get the neighbor of the current node
-            neighbor = graph[currentNode]
-            #sanitize neighbor
-            if backTrackNode in neighbor:
-                neighbor.remove(backTrackNode)
-            #get the maxBandwith connected neighbor of the current node and append to path
-            path.append(self.maxBand(neighbor)[0])
-            backTrackNode = currentNode
-            #replace the current node to the next node in the path
-            currentNode = self.maxBand(neighbor)[0]
+        bandwidthsTuple = sorted(bandwidthsTuple,key=lambda x: x[1])
 
-        return path
+        for (node,bandwidths) in bandwidthsTuple:
+            bandwidthsNode.append(node)
 
+        return bandwidthsNode
+
+    def Reverse(self,clients):
+        clients.reverse()
+        return clients
+
+
+    def modified_bfs_path(self, graph, isp, list_clients):
+        paths = {}
+
+        graph_size = len(graph)
+        priors = [-1]*graph_size
+        search_queue = deque()
+        search_queue.append(isp)
+        while search_queue:
+            node = search_queue.popleft()
+            clients = graph[node]
+            clients = self.maxBandList(clients)
+            clients = self.Reverse(clients)
+            for neighbor in clients:
+                if (priors[neighbor] == -1 and neighbor != isp):
+                    priors[neighbor] = node
+                    search_queue.append(neighbor)
+
+        for client in list_clients:
+            path = []
+            current_node = client
+            while (current_node != -1):
+                path.append(current_node)
+                current_node = priors[current_node]
+            path = path[::-1]
+            paths[client] = path
+
+        return paths
 
     def output_paths(self):
         """
@@ -76,11 +95,10 @@ class Solution:
         BFS will not work
         Polssible solution, run a traversal based on highest bandwidths
         '''
-        # print(self.findPath(root,clients[clients.index(387)],graph))
-        # print(graph[5450])
-
-        bfs_path(graph,root,clients)
+        paths = self.modified_bfs_path(graph,root,clients)
         #387: [2962, 5332, 7757, 1544, 387]
+        #bfs:Revenue: 12219702.0
+        #modified:Revenue: 7807747.0
         
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
